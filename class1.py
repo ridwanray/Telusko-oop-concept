d306d8932a22ac99aa5458e1440e6ee584c36d1f
ridwanray <alabarise@gmail.com>
2021-10-15 03:40:55 +0100

    class

---
+++ class1.py
@@ -0,0 +1,14 @@
+class Computer:
+
+    def config(self):
+        print('i5, 16gb, 17a')
+
+
+
+com1  = Computer()
+com2  = Computer()
+com1.config()
+
+# Computer.config(com1)
+# Computer.config(com2)
+
