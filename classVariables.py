bdaa1edc788c93bcd622bfcf2611de2fa9937b0f
ridwanray <alabarise@gmail.com>
2021-10-15 04:35:51 +0100

    name space

--- classVariables.py
+++ classVariables.py
@@ -1,11 +1,15 @@
 class Car:
-   
+    #class variables is defined in here, i.e outside init 
+    #class name space
+    wheels = 4
     def __init__(self):
-        self.mil = 10
-        self.com = 'BMW'
+        self.mil = 10     # instance variables is defined in init
+        self.com = 'BMW'   # Instance name space
 
 c1 = Car()
 c2 = Car()
 
-print(c1.mil)
-print(c1.com)
+Car.wheels = 23 # updating the wheel class
+Car.anotherwheels = 90 #setting a new class instance
+print(c1.com, c1.mil, c1.wheels, c1.anotherwheels)
+print(c2.com, c2.mil, c2.wheels, c1.anotherwheels)
